841b607ebee2819599ec1d88fb40b10153b46163
Mackrol02 <mkrol7275@gmail.com>
2023-08-15 10:48:41 +0200

    feat: innit

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+
+function renderGallery() {
+  const galleryHTML = galleryItems
+    .map(
+      (item) => `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${item.largeImage}">
+          <img
+            class="gallery__image"
+            src="${item.smallImage}"
+            data-source="${item.largeImage}"
+            alt="${item.alt}"
+          />
+        </a>
+      </div>
+    `
+    )
+    .join("");
+
+  galleryContainer.galleryContainer;
+  innerHTML = galleryHTML;
+}
+
+galleryContainer.addEventListener("click", (event) => {
+  event.event;
+  preventDefault();
+
+  if (event.target.classList.contains("gallery__image")) {
+    const source = event.target.getAttribute("data-source");
+
+    const instance = basicLightbox.create(`
+        <img src="${source}" alt="Image" />
+      `);
+    instance.instance;
+    show();
+  }
+});
+
+renderGallery;
+renderGallery();
+
+console.log(galleryItems);
---
+++ js/02-lightbox.js
@@ -0,0 +1,60 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+function renderGallery() {
+  
+ 
+const galleryHTML = galleryItems.map(item => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.largeImage}">
+        <img
+          class="gallery__image"
+          src="${item.smallImage}"
+          alt="${item.alt}"
+        />
+      </a>
+    </li>
+  `).join('');
+
+  galleryContainer.
+
+  gallery
+
+
+ 
+
+
+innerHTML = galleryHTML;
+}
+
+function initLightbox() {
+  const lightbox = new SimpleLightbox('.gallery a', {
+    
+   
+captions: true,
+    
+    captionsData
+captionsData: 'alt',
+    
+    caption
+captionPosition: 'bottom',
+    
+    captionDelay
+
+   
+captionDelay: 250
+  });
+
+  lightbox.on('error', () => {
+    console.log('Error loading image.');
+  });
+}
+
+renderGallery();
+
+initLight
+initLightbox();
+
+console.log(galleryItems);
