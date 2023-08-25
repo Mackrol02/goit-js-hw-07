2ad36bca51f4f552ffcc7fa2cf92471fa1ab1848
Mackrol02 <mkrol7275@gmail.com>
2023-08-25 10:28:55 +0200

    feat: innit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,12 +8,12 @@ function renderGallery() {
     .map(
       (item) => `
       <div class="gallery__item">
-        <a class="gallery__link" href="preview">
+        <a class="gallery__link" href="large-image.jpg">
           <img
             class="gallery__image"
-            src="original"
-            data-source="preview"
-            alt="description"
+            src="small-image.jpg"
+            data-source="large-image.jpg"
+            alt="Image description"
           />
         </a>
       </div>
